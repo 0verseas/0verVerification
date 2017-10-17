80d53b70ba8bd64c65e6971808d8924f2e8aa790
SafeFly <ast850328@gmail.com>
2017-10-17 22:41:36 +0800

    index cache DOM

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,24 @@ const app = (function () {
   /**
    * cache DOM
    */
-
-
+  const $userIdForm = $('#userIdForm'); // 填入報名序號
+  const $userId = $('#userId'); // 報名序號
+  const $overseasStudentId = $('#overseasStudentId'); // 僑生編號
+  const $name = $('#name'); // 姓名
+  const $engName = $('#engName'); // 英文姓名
+  const $gender = $('#gender'); // 性別
+  const $birthday = $('#birthday'); // 生日
+  const $birthLocation = $('#birthLocation'); // 出生地
+  const $residentLocation = $('#residentLocation'); // 僑居地
+  const $identity = $('#identity'); // 申請身份別
+  const $ruleCodeOfOverseasStudentId = $('#ruleCodeOfOverseasStudentId'); // 規則馬（身份別代碼）
+  const $schoolCountry = $('#schoolCountry'); // 畢業學校國家
+  const $schoolName = $('#schoolName'); // 畢業學校
+  // 確認上傳及報名資料
+  const $applyWay = $('#applyWay'); // 成績採計方式
+  const $diplomaDiv = $('#diplomaDiv'); // 學歷證明 div
+  const $transcriptDiv = $('#transcriptDiv'); // 成績單 div
+  const $verificationDesc = $('#verificationDesc'); // 審核備註
 
   /**
    * init
