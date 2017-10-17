41e1ef416bfdb00e4bf520706e069931cccef89f
SafeFly <ast850328@gmail.com>
2017-10-17 17:35:56 +0800

    login.js 等接資料

---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+const app = (function () {
+
+  /**
+   * cache DOM
+   */
+
+  /**
+   * init
+   */
+
+  _init();
+
+  /**
+   * functions
+   */
+
+  function _init() {
+
+  }
+
+  function login(username, password) {
+    console.log(username + " " + password);
+  }
+
+  return {
+    login
+  }
+
+})();
